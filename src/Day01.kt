61424c88d2abad5b578bedc470c86e0593710706
Tonni Tielens <tonni@tonni.nl>
2023-12-01 08:58:05 +0100

    Add solution for day 1, part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,12 @@
 fun main() {
+    fun firstAndLastDigit(line: String): Int {
+        val first = line.first { it.isDigit() }
+        val last = line.last { it.isDigit() }
+        return "$first$last".toInt()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { firstAndLastDigit(it) }
     }
 
     fun part2(input: List<String>): Int {
@@ -8,10 +14,8 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val input = readInput("Day01.txt")
 
-    val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
 }
