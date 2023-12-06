620c6df1fc548cf30249b4a21c87b4175eebaa65
Tonni Tielens <tonni@tonni.nl>
2023-12-06 08:37:49 +0100

    Solve day 6, part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,10 +1,10 @@
 fun main() {
-    data class RaceInfo(val time: Int, val recordDistance: Int) {
+    data class RaceInfo(val time: Int, val recordDistance: Long) {
         fun nrWaysToBeatRecordDistance(): Int {
             return (1 until time).count {
                 val timeLeft = time - it
                 val speed = it
-                val distance = timeLeft * speed
+                val distance = timeLeft.toLong() * speed.toLong()
                 distance > recordDistance
             }
         }
@@ -14,12 +14,19 @@ fun main() {
         val times = lines[0].substring("Time:".length).split(' ').filter { it.isNotEmpty() }.map { it.toInt() }
         val distances = lines[1].substring("Distance:".length).split(' ').filter { it.isNotEmpty() }.map { it.toInt() }
         check(times.size == distances.size)
-        val timesAndDistances = times.zip(distances).map { RaceInfo(it.first, it.second) }
+        val timesAndDistances = times.zip(distances).map { RaceInfo(it.first, it.second.toLong()) }
         return timesAndDistances.map { it.nrWaysToBeatRecordDistance() }.reduce { acc, n -> acc * n }
+    }
+
+    fun part2(lines: List<String>): Int {
+        val time = lines[0].substring("Time:".length).filter { it != ' ' }.toInt()
+        val distance = lines[1].substring("Distance:".length).filter { it != ' ' }.toLong()
+        return RaceInfo(time, distance).nrWaysToBeatRecordDistance()
     }
 
     val lines = readLines("Day06.txt")
 
 
     part1(lines).println()
+    part2(lines).println()
 }
