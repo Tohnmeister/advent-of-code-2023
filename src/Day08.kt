1a03a286406a7718074431312957ab84724cfe4a
Tonni Tielens <tonni@tonni.nl>
2023-12-09 15:50:51 +0100

    Try to solve day 8, part 2 but fail
    The brute force naive implementation takes several days to finish. There must be a simpler and more efficient way to do this, but I'm having a hard time figuring out what that way is. :-)

--- src/Day08.kt
+++ src/Day08.kt
@@ -18,7 +18,38 @@ fun main() {
 
             currentNode = if (instruction == 'L') nodes[currentNode]!!.first else nodes[currentNode]!!.second
 
-            nrSteps++
+            ++nrSteps
+        }
+
+        return nrSteps
+    }
+
+    fun part2(lines: List<String>): Long {
+        val instructions = lines[0].toList()
+        println(instructions.size)
+        val nodes = lines.subList(2, lines.size).associate {
+            val src = it.substring(0, 3)
+            val left = it.substring(7, 10)
+            val right = it.substring(12, 15)
+
+            Pair(src, Pair(left, right))
+        }
+
+        var nrSteps: Long = 0
+        val currentNodes = nodes.keys.filter { it[2] == 'A' }.toMutableList()
+        while (currentNodes.any { it[2] != 'Z' }) {
+            val idx = nrSteps % instructions.size
+            val instruction = instructions[idx.toInt()]
+
+            if (nrSteps % 1_000_000_000 == 0L) {
+                println("${nrSteps / 1_000_000_000} billion")
+            }
+
+            for ((i, node) in currentNodes.withIndex()) {
+                currentNodes[i] = if (instruction == 'L') nodes[node]!!.first else nodes[node]!!.second
+            }
+
+            ++nrSteps
         }
 
         return nrSteps
@@ -27,4 +58,5 @@ fun main() {
     val lines = readLines("Day08.txt")
 
     part1(lines).println()
+    part2(lines).println()
 }
