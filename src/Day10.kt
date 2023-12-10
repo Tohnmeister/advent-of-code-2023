d7c16a176fde4e88de40d76027e6e2d293fa2329
Tonni Tielens <tonni@tonni.nl>
2023-12-10 10:14:20 +0100

    Find startPos

---
+++ src/Day10.kt
@@ -0,0 +1,18 @@
+fun main() {
+    fun part1(lines: List<String>): Int {
+        // Find S
+        // Then from S, find the loop, by
+        // Going in each direction, while keeping track of all visited positions
+            // If a position is out of the field, then his is NOT the loop
+            // If a position is visited twice, which is not S, then this is NOT the loop
+            // If a position is visited twice, and it is S, then this is the loop
+                // Return length of loop / 2
+        val startPos = lines.asSequence().withIndex().flatMap { (x, line) -> line.withIndex().map { (y, cell) -> Pair(cell, Pair(x, y)) }}.find { it.first == 'S' }!!.second
+
+        return lines.size
+    }
+
+    val lines = readLines("Day10.txt")
+
+    part1(lines).println()
+}
