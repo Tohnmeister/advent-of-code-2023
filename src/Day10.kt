78ac6fd62ece676b667ae6d7ae84be29f465fe29
Tonni Tielens <tonni@tonni.nl>
2023-12-11 18:58:17 +0100

    Make a (slightly over engineered) start with day 10, part 1

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,4 +1,18 @@
 fun main() {
+
+    data class Pipe(val start: Boolean = false, val top: Boolean = false, val right: Boolean = false, val bottom: Boolean = false, val left: Boolean = false)
+
+    val pipeMap = mapOf(
+        '|' to Pipe(top = true, bottom = true),
+        '-' to Pipe(left = true, right = true),
+        'L' to Pipe(top = true, right = true),
+        'J' to Pipe(top = true, left = true),
+        '7' to Pipe(bottom = true, left = true),
+        'F' to Pipe(bottom = true, right = true),
+        '.' to Pipe(),
+        'S' to Pipe(start = true),
+    )
+
     fun part1(lines: List<String>): Int {
         // Find S
         // Then from S, find the loop, by
@@ -7,7 +21,22 @@ fun main() {
             // If a position is visited twice, which is not S, then this is NOT the loop
             // If a position is visited twice, and it is S, then this is the loop
                 // Return length of loop / 2
-        val startPos = lines.asSequence().withIndex().flatMap { (x, line) -> line.withIndex().map { (y, cell) -> Pair(cell, Pair(x, y)) }}.find { it.first == 'S' }!!.second
+        val pipes = lines.map { line -> line.map { pipeMap[it]!! }}
+        val startPos = pipes.asSequence().withIndex().flatMap { (x, line) -> line.withIndex().map { (y, pipe) -> Pair(pipe, Pair(x, y)) }}.find { it.first.start }!!.second
+        val found = false
+        var nrSteps = 0
+        var currentPos = startPos
+        while (!found) {
+            val nextPositions = listOf(
+                Pair(startPos.first, startPos.second - 1), Pair(startPos.first + 1, startPos.second), Pair(startPos.first, startPos.second + 1), Pair(startPos.first - 1, startPos.second)
+            ).filter { (x, y) ->
+                x >= 0 && x < pipes[0].size && y >= 0 && y < pipes.size
+            }
+            for (nextPos in nextPositions) {
+
+            }
+            ++nrSteps
+        }
 
         return lines.size
     }
