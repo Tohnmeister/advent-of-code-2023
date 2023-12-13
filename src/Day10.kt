a62abba146e9d3ab3f849dce333f6458eb1c71a9
Tonni Tielens <tonni@tonni.nl>
2023-12-13 09:16:02 +0100

    First (wrong) attempt at solving day 10, part 2

---
+++ src/Day10.kt
@@ -0,0 +1,107 @@
+package day10;
+
+import day10.Direction.*
+import println
+import readLines
+
+enum class Direction {
+    Top, Right, Bottom, Left;
+
+    fun opposite(): Direction {
+        return when (this) {
+            Top -> Bottom
+            Right -> Left
+            Bottom -> Top
+            Left -> Right
+        }
+    }
+}
+
+class NextPos(val direction: Direction, val x: Int, val y: Int)
+
+fun main() {
+
+    class Pipe(vararg val directions: Direction, val start: Boolean = false) {
+        fun connectsTo(direction: Direction): Boolean = directions.contains(direction)
+    }
+
+    val pipeMap = mapOf(
+        '|' to Pipe(Top, Bottom),
+        '-' to Pipe(Left, Right),
+        'L' to Pipe(Top, Right),
+        'J' to Pipe(Top, Left),
+        '7' to Pipe(Bottom, Left),
+        'F' to Pipe(Bottom, Right),
+        '.' to Pipe(),
+        'S' to Pipe(Top, Right, Bottom, Left, start = true)
+    )
+
+    fun calculateLoop(lines: List<String>): MutableList<Pair<Int, Int>> {
+        val pipes = lines.map { line -> line.map { pipeMap[it]!! } }
+        val startPos = pipes.asSequence().withIndex()
+            .flatMap { (y, line) -> line.withIndex().map { (x, pipe) -> Pair(pipe, Pair(x, y)) } }
+            .find { it.first.start }!!.second
+        var found = false
+        var currentPos = startPos
+        var comingFrom: Direction? = null
+        val loop = mutableListOf<Pair<Int, Int>>()
+        while (!found) {
+            val top = NextPos(Top, currentPos.first, currentPos.second - 1)
+            val right = NextPos(Right, currentPos.first + 1, currentPos.second)
+            val bottom = NextPos(Bottom, currentPos.first, currentPos.second + 1)
+            val left = NextPos(Left, currentPos.first - 1, currentPos.second)
+            val currentPipe = pipes[currentPos.second][currentPos.first]
+
+            val nextPos = sequenceOf(top, right, bottom, left)
+                .filter { nextPos -> nextPos.direction != comingFrom } // The pipe where we came from should not be allowed
+                .filter { nextPos -> nextPos.x >= 0 && nextPos.x < lines[0].length && nextPos.y >= 0 && nextPos.y < lines.size } // The pipe outside the field should not be allowed
+                .filter { currentPipe.connectsTo(it.direction) } // The current pipe should connect to the next pipe
+                .find { nextPos -> pipes[nextPos.y][nextPos.x].connectsTo(nextPos.direction.opposite()) }!! // The next pipe should connect to the current pipe
+
+            currentPos = Pair(nextPos.x, nextPos.y)
+            comingFrom = nextPos.direction.opposite()
+
+            loop.add(currentPos)
+
+            if (pipes[currentPos.second][currentPos.first].start) {
+                found = true
+            }
+        }
+        return loop
+    }
+
+    fun part1(lines: List<String>): Int {
+        val loop = calculateLoop(lines)
+
+        return loop.size / 2
+    }
+
+    fun part2(lines: List<String>): Int {
+        val loop = calculateLoop(lines)
+        var count = 0
+
+        for ((y, line) in lines.withIndex()) {
+            var insideLoop = false
+            for (x in line.indices) {
+                if (loop.contains(Pair(x, y))) {
+                    if (lines[y][x] != '-') {
+                        insideLoop = !insideLoop
+                    }
+                } else {
+                    if (insideLoop) {
+                        ++count
+                    }
+                }
+            }
+        }
+
+        return count
+    }
+
+    val lines = readLines("Day10Test.txt")
+
+    val part1 = part1(lines)
+    //check(part1 == 7063)
+    part1.println()
+    part2(lines).println()
+}
