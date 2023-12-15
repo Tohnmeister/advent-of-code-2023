52fec541d548962db23a1dea3cf62c275b674501
Tonni Tielens <tonni@tonni.nl>
2023-12-15 15:11:42 +0100

    Solve day 12, part 1

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,6 +1,51 @@
+import kotlin.math.pow
+
 fun main() {
     fun part1(lines: List<String>): Int {
-        return lines.size
+        fun isValid(springList: String, sizeList: List<Int>): Boolean {
+            val regexStr = "\\.*" + sizeList.map { List(it) { '#' } }.joinToString("\\.+") { it.joinToString("") } + "\\.*"
+            val regex = regexStr.toRegex()
+
+            val result = regex.matches(springList)
+
+//            if (result) {
+//                println("$springList matches $regex")
+//            } else {
+//                println("$springList does not match $regex")
+//            }
+
+            return result
+        }
+
+        var count = 0
+
+        for (line in lines) {
+            val spacePos = line.indexOf(' ')
+            val springList = line.take(spacePos)
+            val sizeList = line.drop(spacePos + 1).split(',').map { it.toInt() }
+
+            val unknownPositions = springList.asSequence().withIndex().filter { (_, char) -> char == '?' }.map { (idx, _) -> idx }.toList()
+
+            val max = (2.0.pow(unknownPositions.size) - 1).toInt()
+
+            var lineCount = 0
+
+            for (i in 0..max) {
+                val binary = i.toString(2)
+                val stringBuilder = StringBuilder(springList.replace('?', '.'))
+
+                val springPositions = binary.reversed().asSequence().withIndex().filter { it.value == '1' }.map { it.index }.toList()
+                for (springPos in springPositions) {
+                    stringBuilder.setCharAt(unknownPositions[springPos], '#')
+                }
+                if (isValid(stringBuilder.toString(), sizeList)) {
+                    ++lineCount
+                }
+            }
+            count += lineCount
+        }
+
+        return count
     }
 
     val lines = readLines("Day12.txt")
