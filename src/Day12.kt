221d0e53ce9862579b3f56a44ef63c02fe160ffd
Tonni Tielens <tonni@tonni.nl>
2023-12-19 08:07:46 +0100

    Try to solve day 12, part 2 in a completely different way, but fail still
    I'm missing something very simple, but can't figure it out.

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,87 +1,80 @@
-import kotlin.math.pow
+import java.util.stream.Collectors
 
 fun main() {
-    fun calculateSizeGroups(possibility: String): List<Int> {
-        val result: MutableList<Int> = mutableListOf()
-        var lastCount: Int? = null
-        for (char in possibility) {
-            if (char == '#') {
-                if (lastCount == null) {
-                    lastCount = 1
+    fun calculateNrOfPossibilities(springList: String, sizeList: List<Int>, currentSizeIdx: Int = 0): Long {
+        val nrOfExpectedDamagedSprings = sizeList.asSequence().drop(currentSizeIdx).sum() // asSequence
+        val nrOfDamagedSprings = springList.count { it == '#' }
+        if (nrOfDamagedSprings > nrOfExpectedDamagedSprings) {
+            return 0
+        }
+        val currentSize = sizeList[currentSizeIdx]
+        val postSizes = sizeList.asSequence().drop(currentSizeIdx + 1) // asSequence
+        val end = springList.length - 1 - postSizes.sumOf { it + 1 }
+        val lastStart = end - currentSize + 1
+
+        return (0..lastStart).sumOf { startPos ->
+            val partBefore = springList.asSequence().take(startPos) // asSequence
+            val currentPart = springList.asSequence().drop(startPos).take(currentSize) // asSequence
+            val endPos = startPos + currentSize - 1
+            val charAfter = if (endPos == springList.length - 1) null else springList[endPos + 1]
+            val nextCharValid = charAfter != '#'
+
+            if (nextCharValid && partBefore.none { it == '#' } && currentPart.none { it == '.' }) {
+                if (currentSizeIdx == sizeList.size - 1) {
+                    // If there are no more #s behind, then this is a valid possibility. Otherwise, it is not.
+                    if (springList.asSequence().drop(startPos + currentSize + 1).none { it == '#' }) {
+                        1
+                    } else {
+                        0
+                    }
                 } else {
-                    ++lastCount
+                    calculateNrOfPossibilities(
+                        springList.drop(startPos + currentSize + 1),
+                        sizeList,
+                        currentSizeIdx + 1
+                    )
                 }
             } else {
-                if (lastCount != null) {
-                    result.add(lastCount)
-                    lastCount = null
-                }
+                0
             }
         }
-        if (lastCount != null) {
-            result.add(lastCount)
-        }
-
-        return result
     }
 
-    fun calculateNrOfPossibilities(springList: String, sizeList: List<Int>): Int {
-//        println("Checking $springList")
-//        val regex = ("\\.*" + sizeList.map { List(it) { '#' } }.joinToString("\\.+") { it.joinToString("") } + "\\.*").toRegex()
-        val nrOfDamagedSprings = springList.count { it == '#' }
-        val nrOfMissingDamagedSprings = sizeList.sum() - nrOfDamagedSprings
-
-        val unknownPositions =
-            springList.asSequence().withIndex().filter { (_, char) -> char == '?' }.map { (idx, _) -> idx }.toList()
-
-        val max = (2.0.pow(unknownPositions.size) - 1).toInt()
-
-        var lineCount = 0
-
-        for (i in 0..max) {
-            val binary = i.toString(2)
-            if (binary.count { it == '1' } == nrOfMissingDamagedSprings) {
-                val stringBuilder = StringBuilder(springList.replace('?', '.'))
-
-                val springPositions =
-                    binary.reversed().asSequence().withIndex().filter { it.value == '1' }.map { it.index }.toList()
-                for (springPos in springPositions) {
-                    stringBuilder.setCharAt(unknownPositions[springPos], '#')
-                }
-                val possibility = stringBuilder.toString()
-                val actualSizeGroups = calculateSizeGroups(possibility)
-                if (actualSizeGroups == sizeList) {
-                    ++lineCount
-                }
-            }
-        }
-        return lineCount
-    }
-
-    fun part1(lines: List<String>): Int {
+    fun part1(lines: List<String>): Long {
         return lines.sumOf { line ->
             val spacePos = line.indexOf(' ')
             val springList = line.take(spacePos)
             val sizeList = line.drop(spacePos + 1).split(',').map { it.toInt() }
+
             calculateNrOfPossibilities(springList, sizeList)
         }
     }
 
-    fun part2(lines: List<String>): Int {
-        return lines.sumOf { line ->
+    fun part2(lines: List<String>): Long {
+//        return lines.withIndex().sumOf { (idx, line) ->
+//            val spacePos = line.indexOf(' ')
+//            val springList = line.take(spacePos)
+//            val springListUnfolded = List(5) { springList }.joinToString("?")
+//            println("${idx + 1}/${lines.size}: $springListUnfolded")
+//            val sizeList = line.drop(spacePos + 1).split(',').map { it.toInt() }
+//            val sizeListUnfolded = List(5) { sizeList }.flatten()
+//            calculateNrOfPossibilities(springListUnfolded, sizeListUnfolded)
+//        }
+        return lines.withIndex().toList().parallelStream().map { (idx, line) ->
             val spacePos = line.indexOf(' ')
             val springList = line.take(spacePos)
             val springListUnfolded = List(5) { springList }.joinToString("?")
+            println("${idx + 1}/${lines.size}: $springListUnfolded")
             val sizeList = line.drop(spacePos + 1).split(',').map { it.toInt() }
             val sizeListUnfolded = List(5) { sizeList }.flatten()
             calculateNrOfPossibilities(springListUnfolded, sizeListUnfolded)
-        }
+        }.collect(Collectors.summingLong { it })
     }
 
     val lines = readLines("Day12.txt")
 
     val part1 = part1(lines)
     part1.println()
-    check(part1 == 7771)
+    check(part1 == 7771L)
     part2(lines).println()
 }
